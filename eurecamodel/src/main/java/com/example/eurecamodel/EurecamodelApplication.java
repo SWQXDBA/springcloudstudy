d9fe3263e9dccd10d1cedb62a266af8127e32913
SWQXDBA <983110853@qq.com>
2021-09-26 20:57:59 +0800

    1

---
+++ eurecamodel/src/main/java/com/example/eurecamodel/EurecamodelApplication.java
@@ -0,0 +1,13 @@
+package com.example.eurecamodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurecamodelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurecamodelApplication.class, args);
+    }
+
+}
---
+++ eurecamodel/src/test/java/com/example/eurecamodel/EurecamodelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurecamodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurecamodelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
