3185ca71209fc57bcc3e452c9192482719c0b086
SWQXDBA <983110853@qq.com>
2021-09-28 10:15:00 +0800

    112

--- src/main/java/com/example/springcloudstudy/SpringcloudstudyApplication.java
+++ src/main/java/com/example/springcloudstudy/SpringcloudstudyApplication.java
@@ -1,5 +1,7 @@
 package com.example.springcloudstudy;
 
+import com.alibaba.cloud.nacos.ribbon.NacosRule;
+import com.netflix.loadbalancer.IRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,6 +13,12 @@ import org.springframework.web.client.RestTemplate;
 //@EnableEurekaClient
 @SpringBootApplication
 public class SpringcloudstudyApplication {
+
+    //修改负载均衡规则
+    @Bean
+    public IRule getRule(){
+        return new NacosRule();
+    }
 
     @Bean
     @LoadBalanced//用Eureka负载均衡
