fa807253a69aebd5c456ab5599a372fb080048d5
SWQXDBA <983110853@qq.com>
2021-10-01 23:27:32 +0800

    112

---
+++ rabbitmqpublisher/src/main/java/com/example/rabbitmqpublisher/RabbitmqpublisherApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqpublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqpublisherApplication.class, args);
+    }
+
+}
